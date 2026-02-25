4688f6d76bd672a3dbdfad881b6d9e82f2ad2347
Ondesens <vovan.ilin.pskov.128@gmail.com>
2026-02-25 17:53:37 +0300

    Update task_4.py

--- task_4.py
+++ task_4.py
@@ -1,19 +1,18 @@
 str = input("Введите строку: ")
-s=""
-ls=[]
-mn=set()
-k=1
-for i in str :
-    if i !=' ' and i!='' :
-        s+=i
-    if (i == " " or i== str[len(str)-1]) and s!='':
-        mn.add(s)
-        ls.append(s)
-        s =""
-        k+=1
+str = str + " "
+s = ""
+ls = []
+mn = set()
+
+for i in range(len(str)):
+    simvol = str[i]
+    if simvol != " ":
+        s = s + simvol
+    else:
+        if s != "":
+            ls.append(s)
+            mn.add(s)
+            s = ""
 print(ls)
-for  i in mn:
-    print(f'{i} : {ls.count(i)}')
-
-
-
+for j in mn:
+    print(f"{j} : {ls.count(j)}")
