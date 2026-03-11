fee4b2df905f219f90fa2ce8d38ae840339fa8fd
Ondesens <vovan.ilin.pskov.128@gmail.com>
2026-03-11 17:45:11 +0300

    Update task_2.py

--- task_2.py
+++ task_2.py
@@ -1,9 +1,8 @@
-import math
 def simple_number():
     begin_num=2
     while True:
         flag = True
-        for j in range(2 , math.floor((begin_num**0.5))+1):
+        for j in range(2 , round(begin_num**0.5)+1):
             if begin_num % j == 0 :
                 flag = False
         if flag :
@@ -15,8 +14,3 @@ sml_number=simple_number()
 count= int(input("Введите желаемое количество простых чисел : "))
 for _ in range(count):
     print(next(sml_number))
-
-
-
-
-
