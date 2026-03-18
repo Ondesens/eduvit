9f452d7e053f62b72b6d14ea5643589cf1aa56f8
Ondesens <vovan.ilin.pskov.128@gmail.com>
2026-03-18 17:52:54 +0300

    Add files via upload

---
+++ task_2.py
@@ -0,0 +1,16 @@
+def simple_number():
+    begin_num=2
+    while True:
+        flag = True
+        for j in range(2 , round(begin_num**0.5)+1):
+            if begin_num % j == 0 :
+                flag = False
+        if flag :
+            yield begin_num
+
+        begin_num+=1
+
+sml_number=simple_number()
+count= int(input("Введите желаемое количество простых чисел : "))
+for _ in range(count):
+    print(next(sml_number))
